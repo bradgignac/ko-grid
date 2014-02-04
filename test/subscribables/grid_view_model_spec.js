ddacc36176ae07e4423e6fdc7618d6bc65326913
Brad Gignac <bgignac@bradgignac.com>
2014-02-03 21:36:57 -0500

    Toggle sort direction on subsequent clicks.

--- test/subscribables/grid_view_model_spec.js
+++ test/subscribables/grid_view_model_spec.js
@@ -22,40 +22,60 @@ describe('GridViewModel', function () {
   });
 
   describe('#sortColumn', function () {
-    it('initially sorts clicked column in ascending order', function () {
+    beforeEach(function () {
       observable = ko.observableArray([
-        { propertyname: '2' },
-        { propertyname: '3' },
-        { propertyname: '1' }
+        { propertyname: '2', other: 'c' },
+        { propertyname: '4', other: 'b' },
+        { propertyname: '1', other: 'a' }
       ]);
+    });
 
+    it('initially sorts clicked column in ascending order', function () {
       viewModel = new GridViewModel(observable);
       viewModel.sortColumn({ name: 'propertyname' });
 
       expect(viewModel.data()).toEqual([
-        { propertyname: '1' },
-        { propertyname: '2' },
-        { propertyname: '3' }
+        { propertyname: '1', other: 'a' },
+        { propertyname: '2', other: 'c' },
+        { propertyname: '4', other: 'b' }
       ]);
     });
 
     it('maintains sorting when adding new data', function () {
-      observable = ko.observableArray([
-        { propertyname: '2' },
-        { propertyname: '4' },
-        { propertyname: '1' }
-      ]);
-
       viewModel = new GridViewModel(observable);
       viewModel.sortColumn({ name: 'propertyname' });
 
-      observable.push({ propertyname: '3' });
+      observable.push({ propertyname: '3', other: 'd' });
 
       expect(viewModel.data()).toEqual([
-        { propertyname: '1' },
-        { propertyname: '2' },
-        { propertyname: '3' },
-        { propertyname: '4' }
+        { propertyname: '1', other: 'a' },
+        { propertyname: '2', other: 'c' },
+        { propertyname: '3', other: 'd' },
+        { propertyname: '4', other: 'b' }
+      ]);
+    });
+
+    it('toggles sort when clicking column a second time', function () {
+      viewModel = new GridViewModel(observable);
+      viewModel.sortColumn({ name: 'propertyname' });
+      viewModel.sortColumn({ name: 'propertyname' });
+
+      expect(viewModel.data()).toEqual([
+        { propertyname: '4', other: 'b' },
+        { propertyname: '2', other: 'c' },
+        { propertyname: '1', other: 'a' }
+      ]);
+    });
+
+    it('resets to ascending sort when clicking different column', function () {
+      viewModel = new GridViewModel(observable);
+      viewModel.sortColumn({ name: 'propertyname' });
+      viewModel.sortColumn({ name: 'other' });
+
+      expect(viewModel.data()).toEqual([
+        { propertyname: '1', other: 'a' },
+        { propertyname: '4', other: 'b' },
+        { propertyname: '2', other: 'c' }
       ]);
     });
   });
