52d8843b05cd33cb2fec3352270d7a1e9c65fb32
Jigar Solanki <jigar234@gmail.com>
2014-05-05 12:36:12 -0400

    Fix specs for grid

--- test/subscribables/grid_spec.js
+++ test/subscribables/grid_spec.js
@@ -1,6 +1,7 @@
 var grid = require('../../lib/javascripts/subscribables/grid');
 var baseColumn = require('../../lib/javascripts/subscribables/columns/base_column');
 var GridViewModel = require('../../lib/javascripts/subscribables/grid_view_model');
+var TextColumn = require('../../lib/javascripts/subscribables/columns/text_column');
 
 describe('grid', function () {
   var array, viewModel, columns;
@@ -16,10 +17,35 @@ describe('grid', function () {
 
     spyOn(GridViewModel.prototype, 'addColumn');
 
-   viewModel = grid(array, columns);
+    viewModel = grid(array, columns);
 
     expect(viewModel.data()).toBe(array());
+
     expect(viewModel.addColumn).toHaveBeenCalledWith(column1);
     expect(viewModel.addColumn).toHaveBeenCalledWith(column2);
+  });
+
+  describe('columns from observable', function () {
+    var fakeTextColumn;
+
+    beforeEach(function () {
+      array = ko.observableArray([{ 'name': 'foo', 'age': 10 }]);
+
+      fakeTextColumn = new TextColumn('anykey', {});
+
+      spyOn(TextColumn, 'create').andReturn(fakeTextColumn);
+
+      spyOn(GridViewModel.prototype, 'addColumn');
+
+      viewModel = grid(array);
+    });
+
+    it('gets created for each columns', function () {
+      expect(TextColumn.create).toHaveBeenCalledWith('name', {});
+      expect(TextColumn.create).toHaveBeenCalledWith('age', {});
+
+      expect(viewModel.addColumn.calls[0].args[0]).toEqual(fakeTextColumn);
+      expect(viewModel.addColumn.calls[1].args[0]).toEqual(fakeTextColumn);
+    });
   });
 });
--- test/subscribables/grid_view_model_spec.js
+++ test/subscribables/grid_view_model_spec.js
@@ -5,29 +5,84 @@ describe('GridViewModel', function () {
   var viewModel, observableArray;
 
   beforeEach(function () {
-    observableArray = ko.observableArray(['foo', 'bar', 'fun']);
+    observableArray = ko.observableArray([{ 'name' : 'foo' }, { 'name' : 'bar' }, { 'name' : 'fun' }]);
     viewModel = new GridViewModel(observableArray);
   });
 
   it('sets data source', function () {
-    expect(viewModel.data()).toBe(observableArray());
+    expect(viewModel.data()).toEqual([{ 'name' : 'foo' }, { 'name' : 'bar' }, { 'name' : 'fun' }]);
   });
 
   describe('#addColumn', function () {
     var newColumn;
 
     beforeEach(function () {
-      newColumn = new BaseColumn('key', { sortable: true });
-      viewModel = new GridViewModel();
+      newColumn = new BaseColumn('name', { sortable: true });
+      viewModel = new GridViewModel(observableArray);
+
       viewModel.addColumn(newColumn);
     });
 
-    it('adds a column to column array', function () {
-      expect(viewModel.getColumns()[0]).toEqual(newColumn);
+    it('sorts the column', function () {
+      spyOn(newColumn, 'sortState').andReturn('');
+
+      viewModel.sort(newColumn);
+
+      expect(viewModel.data()).toEqual([{ 'name' : 'bar' }, { 'name' : 'foo' }, { 'name' : 'fun' }]);
     });
 
-    it('sorts data', function () {
+    it('reverse sorts the column', function () {
+      spyOn(newColumn, 'sortState').andReturn('ascending');
 
+      viewModel.sort(newColumn);
+
+      expect(viewModel.data()).toEqual([{ 'name' : 'fun' }, { 'name' : 'foo' }, { 'name' : 'bar' }]);
+    });
+
+    it('sets new sort state to ascending if no initial sort state on the column', function () {
+      spyOn(newColumn, 'sortState').andReturn('');
+
+      viewModel.sort(newColumn);
+
+      expect(newColumn.sortState).toHaveBeenCalledWith('ascending');
+    });
+
+    it('sets new sort state to descending if initial sort state is ascending on the column', function () {
+      spyOn(newColumn, 'sortState').andReturn('ascending');
+
+      viewModel.sort(newColumn);
+
+      expect(newColumn.sortState).toHaveBeenCalledWith('descending');
+    });
+  });
+
+  describe('custom sort classes', function () {
+    var newColumn;
+
+    beforeEach(function () {
+      newColumn = new BaseColumn('name', { sortable: true });
+      viewModel = new GridViewModel(
+        observableArray,
+        { 'ascending-sort-css': 'a-css', 'descending-sort-css': 'd-css' }
+      );
+
+      viewModel.addColumn(newColumn);
+    });
+
+    it('sets new sort state to ascending if no initial sort state on the column', function () {
+      spyOn(newColumn, 'sortState').andReturn('');
+
+      viewModel.sort(newColumn);
+
+      expect(newColumn.sortState).toHaveBeenCalledWith('a-css');
+    });
+
+    it('sets new sort state to descending if initial sort state is ascending on the column', function () {
+      spyOn(newColumn, 'sortState').andReturn('a-css');
+
+      viewModel.sort(newColumn);
+
+      expect(newColumn.sortState).toHaveBeenCalledWith('d-css');
     });
   });
 });
